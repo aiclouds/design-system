4547ac55c5e3f812cf7a3f55a74063822fecd49a
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2020-08-31 17:18:49 -0700

    Only use components that won't throw type errors without the latest bugfixes

--- examples/create-react-app-typescript/src/components/App.tsx
+++ examples/create-react-app-typescript/src/components/App.tsx
@@ -1,33 +1,18 @@
-import { Alert, Badge, MonthPicker, getMonthNames } from '@cmsgov/ds-healthcare-gov';
+import { Alert, Button, TextField, getMonthNames } from '@cmsgov/ds-healthcare-gov';
 import React, { SyntheticEvent, useState } from 'react';
-import usflag from '../images/us_flag_small.png';
 
 const LOCALE = 'en';
-const NUM_MONTHS = 12;
-const monthNumbers: number[] = Array.from(new Array(NUM_MONTHS), (_, i) => i + 1);
 const monthNames = getMonthNames(LOCALE);
+console.log(monthNames)
 
 function App() {
-  const [selectedMonths, setSelectedMonths] = useState<number[]>([]);
+  const [text, setText] = useState<string>("");
 
   const handleChange = (event: SyntheticEvent<HTMLInputElement>) => {
-    const month = parseInt(event.currentTarget.value, 10);
-    const months = selectedMonths.slice();
-    if (months.includes(month)) {
-      months.splice(months.indexOf(month), 1);
-    } else {
-      months.push(month);
-    }
-    setSelectedMonths(months.sort((a, b) => a - b));
+    setText(event.currentTarget.value);
   };
 
-  const handleSelectAll = () => {
-    setSelectedMonths(monthNumbers.slice());
-  };
-
-  const handleClearAll = () => {
-    setSelectedMonths([]);
-  };
+  const monthMatches = monthNames.filter(month => text.toLowerCase().includes(month.toLowerCase()));
 
   return (
     <div className="ds-base">
@@ -35,35 +20,28 @@ function App() {
         <h1 className="ds-u-margin--0 ds-u-color--white ds-u-font-size--display ds-u-text-align--center">
           Hello, world!
         </h1>
-        <div className="ds-u-text-align--center">
-          <Badge variation="info" size="big">
-            <img className="c-usa-banner__header-flag" src={usflag} alt="U.S. flag" />
-            &nbsp;CMS Design system
-          </Badge>
-        </div>
       </header>
 
       <div className="ds-l-container">
         <div className="ds-u-measure--base">
-          <MonthPicker
-            name="month-picker"
-            label="Select your favorite months"
-            selectedMonths={selectedMonths}
+          <TextField
+            name="text"
+            label="Enter some text. Maybe a month name?"
+            value={text}
             onChange={handleChange}
-            onSelectAll={handleSelectAll}
-            onClearAll={handleClearAll}
-            locale={LOCALE}
           />
 
-          {selectedMonths.length > 0 && (
-            <Alert heading="Your favorite months" hideIcon>
+          {monthMatches.length > 0 && (
+            <Alert heading="Whoa! Your text contains the following month names" hideIcon>
               <ul>
-                {selectedMonths.map((monthNumber) => (
-                  <li key={monthNumber}>{monthNames[monthNumber - 1]}</li>
+                {monthMatches.map((monthMatches) => (
+                  <li key={monthMatches}>{monthMatches}</li>
                 ))}
               </ul>
             </Alert>
           )}
+
+          <Button>I'm a button!</Button>
         </div>
       </div>
     </div>
